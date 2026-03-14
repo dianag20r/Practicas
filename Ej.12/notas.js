70c2fcd79ed3bfe7a46951b0816934de12369456
Diana Garcia <diana.garcia7670@alumnos.udg.mx>
2026-03-14 17:38:47 -0600

    Ej.12
    Funciones

---
+++ Ej.12/notas.js
@@ -0,0 +1,49 @@
+/*let iterable = [10, 20, 30, 40, 50];
+let suma = 0;
+
+for (let value of iterable) {
+  console.log(`iterable val ${value}`);
+  suma += value;
+}
+console.log(`El total es: ${suma}`);
+
+
+let sumafor = 0;
+for(i=0; i < iterable.length; i++)
+{
+    sumafor += iterable[i];
+}
+
+const myobj = { 
+    a: 1, 
+    b: 2, 
+    c: 3, 
+    zapato: "Grande" 
+};
+for (let prop in myobj) {
+  console.log(`propiedad:${prop} = ${myobj[prop]}`);
+}
+ 
+let iterable="Hola mundo";
+for(let letra of iterable)
+{
+    console.log(letra.toUpperCase());
+}
+
+function capitalizeFirst(word) {
+  //GLOBAL
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+let square = function(num){
+    return num * num;
+};
+let i ="HOla";
+for(let i)
+{
+    console.log
+}
+
+let calculaSigno = fuction(){
+console.log(``);
+}*/
