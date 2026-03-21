259daf7ed4f224a55baf72f1885cd244bfbc8175
Diana Garcia <diana.garcia7670@alumnos.udg.mx>
2026-03-21 17:28:57 -0600

    Ej.12 parte 4
    Quite el alert, use prompt

--- Ej.12/funciones.js
+++ Ej.12/funciones.js
@@ -24,16 +24,16 @@ let calculaSigno = function () {
 
 let adivinaNum = function () {
   let num = Math.floor(Math.random() * 100 + 1);
+  let mensaje = "Adivina un numero (1-100): ";
   for (let i = 1; i < 11; i++) {
-    let respuesta = parseInt(prompt("Adivina un numero (1-100): "));
+    let respuesta = parseInt(prompt(mensaje));
     let n = 10 - i;
-
     if (isNaN(respuesta)) {
-      alert(`Ingresa un numero válido, te quedan ${n} intentos`);
+      mensaje = `Ingresa un numero válido, te quedan ${n} intentos`;
     } else if (num > respuesta) {
-      alert(`El número a adivinar es mayor, te quedan ${n} intentos`);
+      mensaje = `El número a adivinar es mayor, te quedan ${n} intentos`;
     } else if (num < respuesta) {
-      alert(`El número a adivinar es menor, te quedan ${n} intentos`);
+      mensaje = `El número a adivinar es menor, te quedan ${n} intentos`;
     } else {
       alert(`Felicidades acertaste!!, te tomo ${i} intentos`);
       return;
