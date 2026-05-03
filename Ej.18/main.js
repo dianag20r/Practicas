cd4806058678e7c16288e596934784ab4df3c988
Diana Garcia <diana.garcia7670@alumnos.udg.mx>
2026-05-02 21:50:51 -0600

    Ej.18
    Completo, reacomodo de código

--- Ej.18/main.js
+++ Ej.18/main.js
@@ -1,57 +1,52 @@
-//Pueden realizar su propio código o descomentar y ordenar las líneas que se encuentran en cada bloque
+// Paso 1: Obtener los botones por medio de ID
+let btnA = document.querySelector("#A");
+let btnB = document.querySelector("#B");
+let btnC = document.querySelector("#C");
+let myBts = [btnA, btnB, btnC];
 
-//------------------------------------
+// Paso 2: Agregarles el stock, delay y listener a los botones (distinto delay A4s,B3s,C2s)
 
-// let btnB = document.querySelector("#B");
-// let btnC = document.querySelector("#C");
-// // Paso 1: Obtener los botones por medio de ID
-// let btnA = document.querySelector("#A");
-// myBts = [btnA, btnB, btnC];
+let myDelay = 4000;
+myBts.forEach((btnX) => {
+  btnX.stock = 0;
+  btnX.delay = myDelay;
+  btnX.addEventListener("click", changeBtn);
+  myDelay = myDelay - 1000;
+});
 
-//------------------------------------
+//Paso 3: Hacer la promesa y regresar resolve o reject segun un delay aleatorio de 1-5 segundos
 
-// myDelay = 4000;
-// btnX.delay = myDelay;
-// btnX.stock = 0;
-// myBts.forEach((btnX) => {
-// });
-// // Paso 2: Agregarles el stock, delay y listener a los botones (distinto delay A4s,B3s,C2s)
-// btnX.addEventListener("click", changeBtn);
-// myDelay = myDelay - 1000;
+const changeButtonColor = (buttonX) => {
+  return new Promise((resolve, reject) => {
+    buttonX.disabled = true;
+    buttonX.style.backgroundColor = "grey";
+    buttonX.style.color = "black";
 
-//------------------------------------
+    let delay = Math.floor(Math.random() * 4000) + 1000;
+    setTimeout(() => {
+      if (delay <= buttonX.delay) {
+        resolve();
+      } else {
+        reject(`Delay was ${delay} out of ${buttonX.delay}`);
+      }
+    }, delay);
+  });
+};
 
-// buttonX.style.backgroundColor = "grey";
-// if (delay <= buttonX.delay) {
-// // Paso 3: Hacer la promesa y regresar resolve o reject segun un delay aleatorio de 1-5 segundos
-// }
-// const changeButtonColor = (buttonX) => {
-// setTimeout(() => {
-// resolve();
-// } else {
-// buttonX.disabled = true;
-// };
-// });
-// return new Promise((resolve, reject) => {
-// reject(`Delay was ${delay} out of ${buttonX.delay}`);
-// let delay = Math.floor(Math.random() * 4000) + 1000;
-// }, delay);
+// Paso 4: Hacer la función, que modifique el estilo y valor de los botones según el resultado
 
-//------------------------------------
+async function changeBtn() {
+  try {
+    await changeButtonColor(this);
+    this.disabled = false;
+    this.style.backgroundColor = "cyan";
+    this.stock = this.stock + 1;
 
-// this.disabled = false;
-// this.stock = this.stock + 1;
-// try {
-// async function changeBtn() {
-// this.style.backgroundColor = "cyan";
-// } catch (e) {
-// }
-// this.style.color = "white";
-// this.style.backgroundColor = "black";
-// // Paso 4: Hacer la función, que modifique el estilo y valor de los botones según el resultado
-// this.innerText = this.innerText.replace(/[0-9]/g, "") + ` ${this.stock}`;
-// console.log("Error: " + e);
-// }
-// await changeButtonColor(this);
-
-//------------------------------------
+    this.innerText = this.innerText.replace(/[0-9]/g, "") + ` ${this.stock}`;
+  } catch (e) {
+    console.log("Error: " + e);
+    this.style.backgroundColor = "black";
+    this.style.color = "white";
+    this.disabled = false;
+  }
+}
